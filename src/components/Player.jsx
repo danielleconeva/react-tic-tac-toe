9d57fcc67612f13e2f3ff9607b6b5ff165ffd847
danielleconeva <dconewa@gmail.com>
2025-08-21 20:22:55 +0300

    Add board logic with immutable state updates and lifted state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,24 @@
-export default function Player({ name, symbol }) {
+
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+    }
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {isEditing ? (<input type="text" required value={playerName} onChange={handleChange} />) : (<span className="player-name">{playerName}</span>)}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     );
 }
