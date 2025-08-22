22d11a76317a839dd68bf1ad4a724f3ff2645543
danielleconeva <dconewa@gmail.com>
2025-08-22 17:13:02 +0300

    Implement full board logic, move history log and gameplay features

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,39 @@
+import { useState } from 'react';
 
-import { useState } from "react";
-
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({
+    initialName,
+    symbol,
+    isActive,
+    onChangeName,
+}) {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
+
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
     }
+
     function handleChange(event) {
         setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing) {
+        editablePlayerName = (
+            <input type="text" required value={playerName} onChange={handleChange} />
+        );
+
     }
 
     return (
         <li className={isActive ? 'active' : undefined}>
             <span className="player">
-                {isEditing ? (<input type="text" required value={playerName} onChange={handleChange} />) : (<span className="player-name">{playerName}</span>)}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
